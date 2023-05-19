57c5c99860226feb84e1d395de8c6c5f47975e98
HIMZ <himz5221@naver.com>
2023-05-19 09:25:22 +0900

    핳하ㅏ

--- SixsuSaengsu/src/main/java/co/sixsu/app/work/mapper/WorkMapper.java
+++ SixsuSaengsu/src/main/java/co/sixsu/app/work/mapper/WorkMapper.java
@@ -5,11 +5,13 @@ import java.util.List;
 
 import co.sixsu.app.work.domain.DetaProdPlanVO;
 import co.sixsu.app.work.domain.PlanDataVO;
+import co.sixsu.app.work.domain.SorderVO;
 
 public interface WorkMapper {
 
 	public Date getDate();
 	public List<DetaProdPlanVO> getPlan();
 	public List<DetaProdPlanVO> getPlanFilter(PlanDataVO data);
+	public List<SorderVO> getSorder();
 	
 }
