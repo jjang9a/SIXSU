9f1941b58a2655ed2999825d507e5dc23ebef85a
admin <admin@YD05-21.mshome.net>
2023-05-24 17:57:30 +0900

    0524 Go

--- SixsuSaengsu/src/main/java/co/sixsu/app/equipment/web/EquController.java
+++ SixsuSaengsu/src/main/java/co/sixsu/app/equipment/web/EquController.java
@@ -22,8 +22,8 @@ public class EquController {
 	@Autowired EquService equService;
 	
 	
-	// 설비 관리 페이지 - 관리페이지 기능은 /equipment/equCon - 수행.
-	@GetMapping("/equipment/equConpage") 
+	// 관리페이지 기능은 /equipment/equCon - 수행.
+	@GetMapping("/equipment/equConpage")  // 설비 관리 페이지
 	public void equCon() {
 	}
 	
@@ -31,43 +31,32 @@ public class EquController {
 	public void equSearch() {
 	}
     
-	
-	///////////////////
-	
-//	@GetMapping("/equipment/equSearchL") //설비 조회 페이지 리스트
-//	public String equSearchL(Model model){
-//		model.addAttribute("equSearchL", equService.equList());
-//		return "/equipment/equSearch";
-//	}
-//	
-//	@ResponseBody // 설비 조회 리스트 ajax
-//	@RequestMapping("/equipment/equSearchAjax") 
-//	public List<EquConVO> equSearchA() {
-//		 List<EquConVO> list = equService.equList();
-//		 System.out.println(list);
-//		 return list;
-//	}
-	/////////////////////
+	@GetMapping("equipment/equCheck") // 점검관리 페이지
+	public void equcheck() {
+	}
+	// [ 설비 ] - 리스트
 	@ResponseBody
 	@GetMapping("/equipment/equCon") //설비 리스트 ajax
 	public List<EquConVO> equList(){
 		List<EquConVO> list = equService.equConList();
 		return list;
 	}
-	
+	// [설비 관리 페이지] - 등록
 	@ResponseBody // 설비관리(등록) 
 	@PostMapping("/equipment/equAdd")
 	public EquConVO equAdd(EquConVO data) { 
+		System.err.println(data);
 		equService.equAdd(data);
 		return data;
 	}
-	
+	// [설비 관리 페이지] - 수정
 	@ResponseBody // 설비관리(수정) 
 	@PostMapping("/equipment/equUpdate") 
 	public EquConVO equUpdate(EquConVO data) {
 		equService.equUpdate(data);
 		return data;
-	} 
+	}
+	// [설비 관리 페이지] - 삭제
 	@ResponseBody // 설비관리(삭제) 
 	@PostMapping("/equipment/equDel") 
 	public String equdel(@RequestBody EquConVO equCode) {
@@ -78,21 +67,9 @@ public class EquController {
 		  }else {
 			  return "fail";
 		  }
-		  
 	} 
-	 
-	@ResponseBody // 설비조회 상세정보(모달에 데이터를 보내줌)
-	@RequestMapping("/equipment/equInfo") // 설비 조회 상세정보 리스트
-	public List<EquConVO> equInfo(@ModelAttribute EquConVO info) { //담고 담아서
-		String equCode = info.getEquCode(); // info 안에 필드값 EquCode를
-		System.out.println(equCode); // 콘솔창에 뿌려줌
-		List<EquConVO> list = equService.equInfo(equCode); // 리스트 형식 list에 담아서 
-		System.out.println(list);
-		return null; // 모달 창으로 리턴해야함.
 	
-	}
-	
-	
+	// [설비 조회 페이지] - 검색기능
 	@ResponseBody // 검색 기능 / 아작스로 데이터를 요청 리스트로 받음
 	@RequestMapping("/equipment/equFilterAjax")
 	public List equFilterList(@ModelAttribute EquConVO data) {
@@ -102,12 +79,12 @@ public class EquController {
 		return list;
 	}
 	
-	
-	
-	
-	
-//	@GetMapping("equ/equcheck") // 점검관리 페이지
-//	public void equcheck() {
+	// 점검 페이지
+//	@ResponseBody
+//	@GetMapping("/equipment/equCheckList") //설비 리스트 ajax
+//	public List<EquConVO> equCheckList(){
+//		List<EquConVO> list = equService.equCheckList();
+//		return list;
 //	}
 	
 	// 점검 조회
