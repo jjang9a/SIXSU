dee848b8b0351b0f93cc14c85eb9164e63a435e5
J <J@DESKTOP-5RT1EKC>
2023-05-29 02:26:35 +0900

    0528가애

--- SixsuSaengsu/src/main/java/co/sixsu/app/basic/service/ProductService.java
+++ SixsuSaengsu/src/main/java/co/sixsu/app/basic/service/ProductService.java
@@ -6,7 +6,13 @@ import co.sixsu.app.basic.domain.ReceiveVO;
 
 public interface ProductService {
 
+	// 반제품 입고 관리
 	List<ReceiveVO> readySpList(); // 반제품 입고대기 목록
+	List<ReceiveVO> spRecList(); // 반제품 입고목록
 	List<ReceiveVO> spRec(List<ReceiveVO> list); // 반제품 입고
+	
+	// 완제품 입고 관리
+	List<ReceiveVO> readyCpList(); // 반제품 입고대기 목록
+	List<ReceiveVO> cpRecList(); // 반제품 입고목록
 	
 }
