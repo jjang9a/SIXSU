98736450b3844b4b1e5ebefdfb0ecc3757e436b2
Dave_ <Dave_@DESKTOP-NB7KRH1>
2023-05-29 00:38:54 +0900

    Daekyu

--- SixsuSaengsu/src/main/java/co/sixsu/app/sales/serviceimpl/ShipServiceImpl.java
+++ SixsuSaengsu/src/main/java/co/sixsu/app/sales/serviceimpl/ShipServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.sixsu.app.sales.domain.LotVO;
 import co.sixsu.app.sales.domain.OrdVO;
 import co.sixsu.app.sales.mapper.ShipMapper;
 import co.sixsu.app.sales.service.ShipService;
@@ -17,6 +18,19 @@ public class ShipServiceImpl implements ShipService {
 public List<OrdVO> orderingList() {
 	
 	return mapper.orderingList();
+}
+
+@Override //모달창 품목별 lot 재고현황
+public List<LotVO> lotProducts(String keyword) {
+	// TODO Auto-generated method stub
+	return mapper.lotProducts(keyword);
+}
+
+
+@Override//모달창에 자동으로 총수량이 뜨는 기능
+public int totalQt(String keyword) {
+	// TODO Auto-generated method stub
+	return mapper.totalQt(keyword);
 }
 
 }
