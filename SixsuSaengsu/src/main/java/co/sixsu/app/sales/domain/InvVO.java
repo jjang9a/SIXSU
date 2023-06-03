6f7cc0f39098bddc9486d48c4e50de8ef19e7ce4
Dave_ <Dave_@DESKTOP-NB7KRH1>
2023-06-04 00:00:17 +0900

    Daekyu

--- SixsuSaengsu/src/main/java/co/sixsu/app/sales/domain/InvVO.java
+++ SixsuSaengsu/src/main/java/co/sixsu/app/sales/domain/InvVO.java
@@ -8,6 +8,7 @@ import lombok.Data;
 public class InvVO {
 	private String ordId;
 	private String ordDate;
+	private String ordFDate;
 	private String ordStat;
 	private String empId;
 	private String busId;
