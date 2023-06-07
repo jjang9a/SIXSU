d8eae917a5ed1777272d35f2f991bf3f0544e9f3
HIMZ <himz5221@naver.com>
2023-06-08 08:57:11 +0900

    0608 08:57

--- SixsuSaengsu/src/main/java/co/sixsu/app/work/web/WorkController.java
+++ SixsuSaengsu/src/main/java/co/sixsu/app/work/web/WorkController.java
@@ -4,53 +4,52 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import co.sixsu.app.work.service.WorkService;
 
 @Controller
-//@MapperScan(basePackages = "co.sixsu.app.**.mapper")
+@RequestMapping("/work/")
 	public class WorkController {
 	
 		@Autowired WorkService service;
 	
-		@GetMapping("/work/viewPlanPage")
+		@GetMapping("viewPlanPage")
 		public String viewPlanPage() {
 		return "work/viewPlanPage";
 		}
 		
-		@GetMapping("/work/createPlanPage")
+		@GetMapping("createPlanPage")
 		public String createPlanPage() {
 		return "work/createPlanPage";
 		}
-		@GetMapping("/work/viewWorkPage")
+		@GetMapping("viewWorkPage")
 		public String viewWorkPage() {
 			return "work/viewWorkPage";
 		}
-		@GetMapping("/work/workControllPage")
+		@GetMapping("workControllPage")
 		public String workControllPage() {
 		return "work/workControllPage";
 		}
 		
-		@GetMapping("/work/createWorkPage")
+		@GetMapping("createWorkPage")
 		public String createWorkPage(Model model) {
 			String orderCode = service.addWork();
 			model.addAttribute("orderCode", orderCode);
 		return "work/createWorkPage";
 		}
 		
-		@GetMapping("/work/modifyWorkPage")
+		@GetMapping("modifyWorkPage")
 		public String modifyWorkPage(Model model, @RequestParam String data) {
 			data = "20" + data.substring(0,data.lastIndexOf("-"));
 			model.addAttribute("headCode",data);
 			return "work/modifyWorkPage";
 		}
 		
-		
-		
-		@GetMapping("/work/workKiosk")
-		public String xPage() {
-			return "/work/workKiosk";
+		@GetMapping("workKiosk")
+		public String workKiosk() {
+			return "work/workKiosk";
 		}
 }
 
