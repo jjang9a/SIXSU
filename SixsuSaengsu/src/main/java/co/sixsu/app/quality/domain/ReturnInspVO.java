d04bcab8ed17b0c03a6870c25cb8068ecee38e2a
min <jminggg@gmail.com>
2023-06-12 11:18:14 +0900

    0612민경

--- SixsuSaengsu/src/main/java/co/sixsu/app/quality/domain/ReturnInspVO.java
+++ SixsuSaengsu/src/main/java/co/sixsu/app/quality/domain/ReturnInspVO.java
@@ -14,10 +14,12 @@ public class ReturnInspVO {
 	private String empName; // 사원 이름
 	private String retStat; // 반품 진행 상태
 	private String retDate; //반품 일자
+	private String inspDate; // 검사일자
 	private String cpId; // 제품코드
 	private String cpName; // 제품명
-	private String endDate; // 제조일자
+	private String wkEnd; // 제조일자
 	private String expDate; // 유통기한
+	private String cpLotId; // LOT번호
 	
 	private int nsuitQt; // 부적합 수량
 	private int suitQt; // 적합 수량
